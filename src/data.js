8a100369f199c9117f9cd8abfbef0722e300d61f
Digital Nomad <90147636+derkiner@users.noreply.github.com>
2022-07-12 10:54:17 +0300

    React Project

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    name: "John Black",
+    age: 27,
+    image: "https://randomuser.me/api/portraits/men/32.jpg",
+  },
+  {
+    id: 2,
+    name: "Samantha Hudson",
+    age: 32,
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50",
+  },
+  {
+    id: 3,
+    name: "Kim Johnson",
+    age: 28,
+    image: "https://randomuser.me/api/portraits/women/9.jpg",
+  },
+  {
+    id: 4,
+    name: "Jack Donald",
+    age: 29,
+    image: "https://randomuser.me/api/portraits/men/22.jpg",
+  },
+  
+];
+
+
+export default data
